e4a41c99967e27c5770ef08759a1d27f10fa48f4
AndersonPreis <136707826+AndersonPreis@users.noreply.github.com>
2023-06-15 19:35:40 -0400

    Create loginscript.js

---
+++ loginscript.js
@@ -0,0 +1,6 @@
+var email = document.getElementById("email").value;
+var password = document.getElementById("pwd").value;
+
+if(email == "admin" && password == "password"){
+  window.location.href = "admin.html";
+}
