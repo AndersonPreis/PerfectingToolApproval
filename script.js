e7097202751ed15725c8bc039259a35190d22c58
AndersonPreis <136707826+AndersonPreis@users.noreply.github.com>
2023-06-16 11:16:52 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
-var email = document.getElementById("email").value;
-var password = document.getElementById("pwd").value;
 
 loginscript(){
+  var email = document.getElementById("email").value;
+  var password = document.getElementById("pwd").value;
   if(email == "admin" && password == "password"){
     window.location.href = "admin.html";
   }
