b8a56cdd817c5871f57d7d0dd7eeda8b86ce74af
AndersonPreis <136707826+AndersonPreis@users.noreply.github.com>
2023-06-16 12:29:24 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 document.getElementById("login").addEventListener("submit", function(event) {
    event.preventDefault();
+   alert("login initiated");
    var email = document.getElementById("email").value;
    var password = document.getElementById("pwd").value;
    if(email === "admin" && password === "password"){
