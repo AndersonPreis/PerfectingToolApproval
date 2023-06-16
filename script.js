03b28fca2ee0316c415ab3e0d9d1364d6b3acf50
AndersonPreis <136707826+AndersonPreis@users.noreply.github.com>
2023-06-16 14:40:13 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
 document.getElementById("login").addEventListener("submit", function(event) {
-   event.preventDefault();
+   alert("script running");
+   /*event.preventDefault();
    var email = document.getElementById("email").value;
    var password = document.getElementById("password").value;
    if(email === "admin" && password === "password"){
      window.location.href = "admin.html";
    } else {
      alert("Invalid username or password");
-   }
+   }*/
 });
